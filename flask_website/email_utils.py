a1bb5d683ed79b37fa4b04dee0da48455170ce26
Finnley111 <86789658+Finnley111@users.noreply.github.com>
2023-01-28 14:25:41 -0500

    csv file complete, model template created

--- flask_website/email_utils.py
+++ flask_website/email_utils.py
@@ -1,7 +1,12 @@
 from csv import writer
 
-email = "APPLICATION"
+email = ''''''
 email = email.lower()
+email = email.replace(' ', ',')
+
+result = 0
+
+
 
 filename = "email_data.csv"
 fobj = open(filename, "r")
@@ -10,28 +15,26 @@ new_row = {}
 first_line = ""
 for line in fobj:
     first_line = line
+    break
+
+fobj.close()
 
 fl_list = line.split(',')
 
 for word in fl_list:
     new_row[word] = email.count(word)
+print(new_row)
+new_row['result\n'] = result
 
-list_values = new_row.values()
+list_values = [new_row.values()]
 
-fobj.close()
+
 
 ################################################
 # write
 ################################################
-with open('email_data.csv', 'a') as f_object:
- 
-    # Pass this file object to csv.writer()
-    # and get a writer object
-    writer_object = writer(f_object)
- 
-    # Pass the list as an argument into
-    # the writerow()
-    writer_object.writerow(list_values)
- 
-    # Close the file object
-    f_object.close()
+with open("email_data.csv", "a", newline='') as file:
+    # Create a CSV writer object
+    writer = writer(file)
+    # Write the values to the CSV file
+    writer.writerows(list_values)
--- flask_website/hello.py
+++ flask_website/hello.py
@@ -3,7 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return parse_email()
     return 'hi world!'
 
 if __name__ == "__main__":
