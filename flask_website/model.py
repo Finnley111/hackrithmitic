138b68234b4758f90af66e5f48ec60100ca686fe
Finnley111 <86789658+Finnley111@users.noreply.github.com>
2023-01-28 15:42:20 -0500

    model can be applied to inputted text

--- flask_website/model.py
+++ flask_website/model.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import new_email
 
 dataset = pd.read_csv(r'email_data.csv')
 X = dataset.iloc[:, :-1].values
@@ -9,23 +10,44 @@ y = dataset.iloc[:, -1].values
 # spliting the dataset
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-print(y_test)
 
 # training the multiple linear regression model
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 
-# predicting the test set results
-y_pred = regressor.predict(X_test)
-np.set_printoptions(precision=2)
-print('\n', np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
-print(y_pred)
-y_pred_round = []
-for ele in y_pred:
-    if ele >= 0.5:
-        y_pred_round.append(1)
-    else:
-        y_pred_round.append(0)
+def get_result(email_data, regressor):
+    email_data = np.array([email_data])
+    # predicting the test set results
+    y_pred = regressor.predict(email_data)
+    np.set_printoptions(precision=2)
 
-print(y_pred_round)
+    if y_pred >= 0.5:
+        return 1
+    else:
+        return 0
+    
+
+
+#####################################
+# TEST
+#####################################
+
+# email = '''Hi Finnley,
+
+# Congratulations, you have passed the screening stage for the Data Scientist (Risk Research) Intern position and will move on to the next step of the process, our technical test.
+
+# The test will take at most 120 minutes. Please ensure you use the same email on the test as you did on your application so we can credit you with your results. You will receive an invitation from HackerRank shortly after receiving this email.
+
+# Please ensure you take the test within 72 hours of receiving it. The system takes some time to send the test link, but if you do not receive it by the end of the day, please respond to this email, and I will re-send it.
+
+# Any questions? Please join our internship LinkedIn Network for the fastest response time, and post your questions in the group.
+
+# Best of luck!
+
+# Eleanor Hawkins
+# Campus Recruitment Team
+# GeoComply'''
+
+# test = new_email.email_data(email)
+# print(get_result(test, regressor))
--- flask_website/new_email.py
+++ flask_website/new_email.py
@@ -20,22 +20,22 @@ def email_data(text):
     del new_row['result\n']
 
     list_values = new_row.values()
-    print(len(list_values))
+    
     return list(list_values)
 
-email = '''Hi Finnley,
+# email = '''Hi Finnley,
 
-Congratulations, you have passed the screening stage for the Data Scientist (Risk Research) Intern position and will move on to the next step of the process, our technical test.
+# Congratulations, you have passed the screening stage for the Data Scientist (Risk Research) Intern position and will move on to the next step of the process, our technical test.
 
-The test will take at most 120 minutes. Please ensure you use the same email on the test as you did on your application so we can credit you with your results. You will receive an invitation from HackerRank shortly after receiving this email.
+# The test will take at most 120 minutes. Please ensure you use the same email on the test as you did on your application so we can credit you with your results. You will receive an invitation from HackerRank shortly after receiving this email.
 
-Please ensure you take the test within 72 hours of receiving it. The system takes some time to send the test link, but if you do not receive it by the end of the day, please respond to this email, and I will re-send it.
+# Please ensure you take the test within 72 hours of receiving it. The system takes some time to send the test link, but if you do not receive it by the end of the day, please respond to this email, and I will re-send it.
 
-Any questions? Please join our internship LinkedIn Network for the fastest response time, and post your questions in the group.
+# Any questions? Please join our internship LinkedIn Network for the fastest response time, and post your questions in the group.
 
-Best of luck!
+# Best of luck!
 
-Eleanor Hawkins
-Campus Recruitment Team
-GeoComply'''
-print(email_data(email))
+# Eleanor Hawkins
+# Campus Recruitment Team
+# GeoComply'''
+# print(email_data(email))
